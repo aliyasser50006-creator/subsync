601f1595840623be4c12e31202b31a71f849d87d
aliyasser50006-creator <aliyasser50006@gmail.com>
2026-08-03 17:06:31 +0300

    Delete app directory

--- app/(app)/library/[id]/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { VideoDetailSkeleton } from '@/components/library/video-detail/video-detail-skeleton';
-
-export default function LibraryDetailLoading() {
-  return <VideoDetailSkeleton />;
-}
--- app/(app)/my-videos/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function MyVideosRedirect() {
-  redirect('/media-manager');
-}
